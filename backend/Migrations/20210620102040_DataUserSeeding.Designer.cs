43cb831db0f12b502d219378b9aedc12f77665fd
Mansour-Aldundur <mansour.aldundur@tuwaiq.edu.sa>
2021-06-20 13:24:36 +0300

    add: data seeding to the database

--- backend/Migrations/20210620102040_DataUserSeeding.Designer.cs
+++ backend/Migrations/20210620102040_DataUserSeeding.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using backend.Models.Data;
 
 namespace backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210620102040_DataUserSeeding")]
+    partial class DataUserSeeding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
