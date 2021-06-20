d032629a7997b950372740d9a550f4fdccf26cc8
1Riyad <riyad.alghamdi@tuwaiq.edu.sa>
2021-06-20 14:18:42 +0300

    deploy to netlify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,20 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import HomeView from './views/HomeView'
 import ErrorView from './views/ErrorView'
+import CreateView from './views/CreateView'
+import NavBar from './Components/NavBar'
+import Repo from './Components/Repo'
+import Profile from './Components/Profile'
 
 export default function App() {
-  return (
+  return (  
     <Router>
+      <NavBar/>
       <Switch>
         <Route path="/" component={HomeView} exact />
+        <Route path="/create" component={CreateView} exact />
+        <Route path="/Repo" component={Repo} exact />
+        <Route path="/Profile" component={Profile} exact />
         <Route component={ErrorView} />
       </Switch>
     </Router>
